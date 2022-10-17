3bc52e442218ef5ae587eda0aecf67681fbab326
mathiascoutant <mathiascoutant@icloud.com>
2022-10-17 22:14:39 +0200

    c good

--- main/server.go
+++ main/server.go
@@ -12,7 +12,7 @@ func main() {
 	fs := http.FileServer(http.Dir("../static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	port := "https://neeft-back.herokuapp.com"
+	port := "127.0.0.1:8000"
 	println("Server listen on", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
