34c8492a0f250e3d491c7faf0d05a2a22ad665ee
Grégory DAVID <gregory.david@ynov.com>
2022-11-19 16:36:51 +0100

    Re-wrote the login code for better visibility

--- controllers/connect.go
+++ controllers/connect.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"database/sql"
+	"golang.org/x/crypto/bcrypt"
 	"neeft_back/models"
 	"net/http"
 
@@ -13,27 +14,36 @@ import (
 func Connect(c *gin.Context) {
 	// Open the database
 	db, _ := sql.Open("sqlite3", "./bdd.db")
+	defer db.Close()
 
-	askedUsername := c.PostForm("username")
-	askedPassword := c.PostForm("password")
+	inUsername := c.PostForm("username")
+	inPassword := c.PostForm("password")
 
-	if askedUsername != "" {
-		if askedPassword != "" {
-			row := db.QueryRow("select * from users where username=? and password=?", askedUsername, askedPassword)
-			user := new(models.User)
-			err := row.Scan(&user.Id, &user.Username, &user.Password, &user.FirstName, &user.LastName, &user.Email, &user.EmailVerifiedAt)
-			if err != nil {
-				c.JSON(http.StatusForbidden, gin.H{"message": "Username or password is invalid", "code": 401})
-				return
-			} else {
-				c.JSON(http.StatusOK, gin.H{"username": askedUsername, "id": user.Id, "password": askedPassword, "code": 200})
-			}
-		} else {
-			c.JSON(http.StatusForbidden, gin.H{"message": "empty password", "code": 500})
-		}
-	} else {
-		c.JSON(http.StatusForbidden, gin.H{"message": "empty Username", "code": 500})
+	if len(inUsername) == 0 {
+		c.JSON(http.StatusForbidden, gin.H{"message": "Please enter a valid username", "code": 403})
+		return
+	} else if len(inPassword) == 0 {
+		c.JSON(http.StatusForbidden, gin.H{"message": "Please enter a non-null password", "code": 403})
+		return
 	}
 
-	db.Close()
+	row := db.QueryRow("select * from users where username=?", inUsername)
+
+	user := new(models.User)
+	hashedPassword := ""
+
+	err := row.Scan(&user.Id,
+		&user.Username,
+		&hashedPassword,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.EmailVerifiedAt)
+
+	if err != nil || bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inPassword)) != nil {
+		c.JSON(http.StatusForbidden, gin.H{"message": "Username or password is invalid", "code": 401})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"username": inUsername, "id": user.Id, "code": 200})
+	}
 }
