91931ee1e6a302f6acf86ffd4c28242f4191abac
Cacahuète Dev <gattaquhs28@gmail.com>
2022-11-23 14:04:27 +0100

    Converted /new_team to JSON POST requests

--- utils/errors.go
+++ utils/errors.go
@@ -19,3 +19,18 @@ var UsernameOrPasswordInvalidError = Error{
 	Code:    "ERR04",
 	Message: "Username or password is invalid",
 }
+
+var TeamWithSameNameExistsError = Error{
+	Code:    "ERR05",
+	Message: "A team with the same name already exist",
+}
+
+var InvalidInfosProvided = Error{
+	Code:    "ERR06",
+	Message: "Invalid informations provided",
+}
+
+var DatabaseError = Error{
+	Code:    "ERR07",
+	Message: "Database Error",
+}
