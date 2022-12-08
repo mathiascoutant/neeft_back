72112450d0021f2c931d296de592b00a8034a439
Cacahuète Dev <gattaquhs28@gmail.com>
2022-12-08 15:49:28 +0100

    tasks #89: la même team ne peut pas repostuler une deuxième fois + tests

--- app/controllers/tournament/tournamentPendingRequest.go
+++ app/controllers/tournament/tournamentPendingRequest.go
@@ -20,26 +20,35 @@ type TournamentTeamsRequest struct {
 // SendPendingRequest is used to send a pending request to join a tournament by a team
 func SendPendingRequest(c *fiber.Ctx) error {
 	var request TournamentTeamsRequest
-	id, _ := c.ParamsInt("id")
+	tournamentId, _ := c.ParamsInt("id")
 
 	if err := c.BodyParser(&request); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
 	var tournament tournaments.Tournament
-	err := FindTournament(id, &tournament)
+	err := FindTournament(tournamentId, &tournament)
 	if err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
 	var team teams.Team
-	err = teams2.FindTeam(id, &team)
+	err = teams2.FindTeam(int(request.TeamId), &team)
 	if err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
+	// Check if the team has already sent a pending request
+	var potentialRequest tournaments.TournamentTeams
+
+	if err := database.Database.Db.Where("team_id = ?", request.TeamId).First(&potentialRequest).Error; err == nil {
+		if potentialRequest.Status == tournaments.StatusPending {
+			return c.Status(400).JSON("This team has already sent a pending request")
+		}
+	}
+
 	finalItem := tournaments.TournamentTeams{
-		TournamentId: uint(id),
+		TournamentId: uint(tournamentId),
 		TeamId:       request.TeamId,
 		Status:       0,
 		Tournament:   tournament,
